acdec8cff0a1ccb0e8582752ee1382ed63d5e2f8
antonioh <antonioh@LES007800.corp.capgemini.com>
2019-12-03 12:23:31 +0100

    Se cambia estilo de pagina apuesta.html

--- Formula2019/src/main/java/com/dawes/ApuestasF1Application.java
+++ Formula2019/src/main/java/com/dawes/ApuestasF1Application.java
@@ -9,11 +9,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ApuestasF1Application extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
+			
 			SpringApplication.run(ApuestasF1Application.class, args);
 		
 			Persistence.generateSchema("formula",null);
 			
-		
 	}
 
 }
